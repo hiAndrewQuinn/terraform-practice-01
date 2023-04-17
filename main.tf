24f5f1da8bdab8a76164ac88ad20775548705ce3
Andrew Quinn <53230903+hiAndrewQuinn@users.noreply.github.com>
2023-04-17 17:54:03 +0300

    feat: A tiny bit of terraform

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  # Default for us-east-2. Spins up an Ubuntu 22.04 box.
-  ami           = "ami-0fb653ca2d3203ac1"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "terraform-example"
-  }
-}
+# resource "aws_instance" "example" {
+#   # Default for us-east-2. Spins up an Ubuntu 22.04 box.
+#   ami           = "ami-0fb653ca2d3203ac1"
+#   instance_type = "t2.micro"
+# 
+#   tags = {
+#     Name = "terraform-example"
+#   }
+# }
